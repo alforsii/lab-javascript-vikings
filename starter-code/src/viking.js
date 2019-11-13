cf14a1cb6a3032ba486815acb704c773d26bb5fe
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2019-11-12 19:02:34 -0500

    update-done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -8,7 +8,7 @@ class Soldier {
     return this.strength;
   }
   receiveDamage(theDamage) {
-    this.health = this.health - theDamage;
+    this.health -= theDamage;
   }
 }
 
@@ -65,7 +65,8 @@ class War {
     let randomV = Math.floor(Math.random() * this.vikingArmy.length);
     let randomVik = this.vikingArmy[randomV];
 
-    let saxonSoldier = randomSax.receiveDamage(randomVik.strength);
+    let saxonSoldier = randomSax.receiveDamage(randomVik.attack());
+    // let saxonSoldier = randomSax.receiveDamage(randomVik.strength);
     if (randomSax.health <= 0) this.saxonArmy.splice(randomS, 1);
     return saxonSoldier;
   }
@@ -75,16 +76,17 @@ class War {
     let randomV = Math.floor(Math.random() * this.vikingArmy.length);
     let randomVik = this.vikingArmy[randomV];
 
-    let vikingSoldier = randomVik.receiveDamage(randomSax.strength);
+    let vikingSoldier = randomVik.receiveDamage(randomSax.attack());
+    // let vikingSoldier = randomVik.receiveDamage(randomSax.strength);
     if (randomVik.health <= 0) this.vikingArmy.splice(randomV, 1);
     return vikingSoldier;
   }
   showStatus() {
     if (this.vikingArmy <= 0)
-      return 'Saxons have fought for their lives and survived another day...';
+      return "Saxons have fought for their lives and survived another day...";
     else if (this.saxonArmy <= 0)
-      return 'Vikings have won the war of the century!';
-    else return 'Vikings and Saxons are still in the thick of battle.';
+      return "Vikings have won the war of the century!";
+    else return "Vikings and Saxons are still in the thick of battle.";
 
     // // My solution is below --->>>
     // if (this.saxonArmy.length === 0)
@@ -105,9 +107,9 @@ const saxon3 = new Saxon(300, 300);
 newWar.addSaxon(saxon1);
 newWar.addSaxon(saxon2);
 newWar.addSaxon(saxon3);
-const viking1 = new Viking('Ash', 100, 100);
-const viking2 = new Viking('Kev', 200, 200);
-const viking3 = new Viking('Sam', 300, 300);
+const viking1 = new Viking("Ash", 100, 100);
+const viking2 = new Viking("Kev", 200, 200);
+const viking3 = new Viking("Sam", 300, 300);
 newWar.addViking(viking1);
 newWar.addViking(viking2);
 newWar.addViking(viking3);
@@ -118,15 +120,15 @@ newWar.addViking(viking3);
 // console.log('newWar.vikingArmy', newWar.vikingArmy);
 
 newWar.saxonAttack();
-console.log('newWar.saxonAttack()', newWar.saxonAttack());
+console.log("newWar.saxonAttack()", newWar.saxonAttack());
 newWar.vikingAttack();
-console.log('newWar.vikingAttack()', newWar.vikingAttack());
+console.log("newWar.vikingAttack()", newWar.vikingAttack());
 newWar.saxonAttack();
-console.log('newWar.saxonAttack()', newWar.saxonAttack());
-console.log('newWar.vikingArmy after attack', newWar.vikingArmy);
+console.log("newWar.saxonAttack()", newWar.saxonAttack());
+console.log("newWar.vikingArmy after attack", newWar.vikingArmy);
 
 newWar.showStatus();
-console.log('newWar.showStatus()', newWar.showStatus());
+console.log("newWar.showStatus()", newWar.showStatus());
 
 // //Every time you ran the code it will log every time random output like:
 
